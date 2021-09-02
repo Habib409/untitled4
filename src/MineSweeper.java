e167dc500c90f27fc2ba07df1e0e2b1ec1856687
Habib409 <74184133+Habib409@users.noreply.github.com>
2021-09-02 23:34:02 +0200

    test

---
+++ src/MineSweeper.java
@@ -0,0 +1,22 @@
+public class MineSweeper {
+    private String[] field;
+    public MineSweeper(String[] field) {
+        this.field = field;
+    }
+    
+    public String[] getHints() {
+        String[] hints = new String[field.length];
+        for (int i = 0; i < field.length; i++) {
+            String rowHint = "";
+            for (int j = 0; j < this.field[i].length(); j++) {
+                if (this.field[i].charAt(j) == '*' ) {
+                    rowHint += "*";
+                } else {
+                    rowHint += "0";
+                }
+            }
+            hints[i] = rowHint;
+        }
+        return hints;
+    }
+}
---
+++ test/MineField.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+import java.lang.ref.SoftReference;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MineField {
+    @Test
+    void shouldEmptyField() {
+        String[] field = {"."};
+        String[] expected = {"0"};
+        assertArrayEquals(expected, new MineSweeper(field).getHints());
+    }
+
+    @Test
+    void shouldShowTallMineFields() {
+        String[] field = {".", ".", ","};
+        String[] expected = {"0", "0", "0"};
+        assertArrayEquals(expected, new MineSweeper(field).getHints());    }
+
+    @Test
+    void shouldShowWideMineFields() {
+        String[] field = {"....", "....", "...."};
+        String[] expected = {"0000", "0000", "0000"};
+        assertArrayEquals(expected, new MineSweeper(field).getHints());
+    }
+
+    @Test
+    void shouldShowFullMineFields() {
+        String[] field = {"****", "****", "****"};
+        String[] expected = {"****", "****", "****"};
+        assertArrayEquals(expected, new MineSweeper(field).getHints());
+    }
+
+
+}
+
